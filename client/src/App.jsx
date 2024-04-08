28b1fc62993010c8806dedb1562432c2ce30fa9f
Hielo Elemental <136840526+HieloElemental@users.noreply.github.com>
2024-04-08 21:54:38 +0000

    project initialization

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import TipTap from "./components/TipTap"
+
+function App() {
+  return (
+    <>
+      <form>
+        <TipTap />
+      </form>
+    </>
+  )
+}
+
+export default App
